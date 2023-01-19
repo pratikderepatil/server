4239deb299fcbb9ea13c22784f24e41d8508d36e
pratikderepatil <pratikdere333@gmail.com>
2023-01-19 12:57:08 +0530

    Bcrypt the password done | stored password in hashed format

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,8 +1,16 @@
 const express = require("express");
-
+const bcrypt = require("bcrypt");
 const UserModel = require("../models/user.model");
 
 const app = express.Router();
+
+const bcryptPassword = async (password) => {
+	return await bcrypt.hash(password, 10);
+};
+
+const compareBcryptPassword = async (password, passwordHash) => {
+	return await bcrypt.compare(password, passwordHash);
+};
 
 app.post("/signup", async (req, res) => {
 	let { email, password } = req.body;
@@ -15,7 +23,10 @@ app.post("/signup", async (req, res) => {
 				.send("This email is already in use try with other email.");
 		}
 
-		let newUser = new UserModel({ email, password });
+		let newUser = new UserModel({
+			email,
+			password: await bcryptPassword(password),
+		});
 		await newUser.save();
 		return res.status(201).send(newUser);
 	} catch (e) {
@@ -26,8 +37,8 @@ app.post("/signup", async (req, res) => {
 app.post("/login", async (req, res) => {
 	const { email, password } = req.body;
 
-	const user = await UserModel.findOne({ email, password });
-	if (user) {
+	const user = await UserModel.findOne({ email });
+	if (user && (await compareBcryptPassword(password, user.password))) {
 		return res.status(200).send({
 			message: "Login Success",
 			email: user.email,
